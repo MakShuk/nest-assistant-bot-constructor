e823ad27b8b270ad7fac9ad0ac430067304fccb3
Maksim Shuklin <88307988+MakShuk@users.noreply.github.com>
2024-08-22 15:47:54 +0300

    feat: create project and add configuration module

---
+++ src/openai-assistant/openai-assistant.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Get, Post, HttpCode } from '@nestjs/common';
+import { OpenaiAssistantService } from './openai-assistant.service';
+
+interface AssistantParams {
+  name: string;
+  instructions: string;
+}
+
+interface Message {
+  content: string;
+}
+
+interface File {
+  path: string;
+  id: string;
+}
+
+@Controller('openai-assistant')
+export class OpenaiAssistantController {
+  constructor(
+    private readonly openaiAssistantService: OpenaiAssistantService,
+  ) {}
+
+  @Post('create')
+  async createAssistant(
+    @Body()
+    assistantParams: AssistantParams,
+  ) {
+    return await this.openaiAssistantService.createAssistant(assistantParams);
+  }
+
+  @Get('all')
+  async getAssistantConfig() {
+    return await this.openaiAssistantService.getAssistantConfig();
+  }
+
+  @Post('start')
+  async startDialog(
+    @Body()
+    message: Message,
+  ) {
+    return await this.openaiAssistantService.startDialog(message.content);
+  }
+
+  @Post('reset-chat')
+  @HttpCode(204)
+  resetChat() {
+    return this.openaiAssistantService.resetThread();
+  }
+
+/*   @Post('upload-file')
+  fileUploads(
+    @Body()
+    file: File,
+  ) {
+    return this.openaiAssistantService.uploadFile(file.path, file.id);
+  } */
+}
